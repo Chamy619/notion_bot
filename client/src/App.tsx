ed9875c3f6958e70f45ba2457e765ebf0534e219
Chamy <yd95069506@naver.com>
2021-06-07 16:58:08 +0900

    네이버 로그인 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import {
 import LoginPage from './components/LoginPage/LoginPage';
 import Editor from './components/Editor/Editor';
 import Auth from './hoc/auth';
+import NaverLoginGetProfile from './components/LoginPage/NaverLoginGetProfile';
 
 const App: React.FC = () => {
   return (
@@ -14,6 +15,9 @@ const App: React.FC = () => {
       <Router>
         <Route exact path="/" component={Auth(LoginPage, false)} />
         <Route exact path="/editor" component={Auth(Editor, true)} />
+        <Route path="/naver-login">
+          <NaverLoginGetProfile />
+        </Route>
       </Router>
     </div>
   )
